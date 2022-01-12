191abf7d3ba8a7a70cc079cb6593c4482575b8ab
rewpt <zpermack@gmail.com>
2022-01-12 23:36:59 +0000

    create route for about index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  get 'about', to: 'about#index'
 
   namespace :admin do
     root to: 'dashboard#show'
