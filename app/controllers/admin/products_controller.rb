4fdfb649b5a0c011c331918fa2a961177b392835
rewpt <zpermack@gmail.com>
2022-01-12 16:27:36 +0000

    add authentication feature for admin pages from env vars

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASS']
   def index
     @products = Product.order(id: :desc).all
   end
