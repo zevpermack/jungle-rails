15fadfa89f01abb226b21eca4ba45c2ec3023cff
rewpt <zpermack@gmail.com>
2022-01-13 21:39:25 +0000

    create s index, new, create defs and routes for admin/categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,27 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Categories.order(id: :desc).all
+  end
 
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+  
+  def new
+    @category = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+ 
 
 end
